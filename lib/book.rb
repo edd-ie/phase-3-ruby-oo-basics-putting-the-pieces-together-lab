a17d2af52bdfb37c5f7433438a0099fc355ff6b9
edd-ie <ogendoeddie@gmail.com>
2023-05-18 15:37:16 +0300

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
+class Book
+    attr_accessor :title, :author, :pages, :genre
+    def initialize(title)
+        @title = title
+    end
+    def title
+        @title
+    end
+    def author=(val)
+        @author = val
+    end
+    def page_count=(val)
+        @pages = val
+    end
+    def page_count
+        @pages
+    end
+    def genre=(val)
+        @genre = val
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
+end
 
+test  = Book.new("test")
+test.page_count = 272
+puts test.page_count
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,46 @@
 # Make your shoe class here!
+class Shoe
+    def initialize(brand)
+        @brand = brand 
+    end
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def brand=(brand)
+        @brand = brand
+    end
+    def color=(color)
+        @color = color
+    end
+    def size=(size)
+        @size = size
+    end
+    def material=(material)
+        @material = material
+    end
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+    def brand
+        @brand
+    end
+    def color
+        @color
+    end
+    def siz
+        @size
+    end
+    def material
+        @material
+    end
+    def condition
+        @condition
+    end
+
+
+end
